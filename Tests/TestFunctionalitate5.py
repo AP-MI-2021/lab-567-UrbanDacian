835170d6b3a566d6f73a7049c83b3559d374c8be
UrbanDacian <dacianurban13@gmail.com>
2021-11-09 00:27:55 +0200

    Teste

--- Tests/TestFunctionalitate5.py
+++ Tests/TestFunctionalitate5.py
@@ -1,13 +1,13 @@
 from Domain.Cheltuiala import getPret
 from Logic.CRUD import adaugaCheltuiala, getById
-from Logic.Functionalitate2 import ieftinireCheltuiala
+from Logic.Functionalitate5 import suma
 
 
-def testIeftinireCheltuiala():
+def testSuma():
     lista = []
     lista = adaugaCheltuiala(1, "Ion", "Business", 300, "Da", lista)
     lista = adaugaCheltuiala(2, "Marcel", "Economy", 200, "Nu", lista)
-    ieftinireCheltuiala(1/2, lista)
-    assert len(lista) == 2
-    assert getPret(getById(1, lista)) == 150
-    assert getPret(getById(2, lista)) == 100
+
+    suma(lista)
+    
+    assert getPret(getById(2, lista)) == 200
