1b88e998313860bb4067d86f5fafe29c7690cd5f
UrbanDacian <dacianurban13@gmail.com>
2021-11-09 00:27:36 +0200

    Functionalitate5

--- Logic/Functionalitate5.py
+++ Logic/Functionalitate5.py
@@ -2,21 +2,19 @@ from Domain.Cheltuiala import getNume, getId, getPret
 
 
 def suma(lista):
-    '''
-    Afișarea sumelor prețurilor pentru fiecare nume.
-    :param lista: lista de dicitonare
-    :return: suma pentru fiecare nume din lista
-    '''
-    arr = []
-    brr = []
-    for i in range(len(lista)):
-        arr[i] = 0
-    for j in range(len(lista)):
-        brr[j] = 1
+    """
+    Functia returneaza suma preturilor penru fiecare Nume
+    :param lista: lista cu rezervari
+    :return: o lista cu suma preturilor pentru fiecare nume
+    """
+    rezultat = {}
     for cheltuiala in lista:
-        for cheltuiala2 in lista:
-            if getNume(cheltuiala) == getNume(cheltuiala2) and brr[cheltuiala2] == 1:
-                arr[getId(cheltuiala)] += getPret(cheltuiala2)
-                brr[getId(cheltuiala2)] = 0
+        nume = getNume(cheltuiala)
+        pret = getPret(cheltuiala)
+        if nume in rezultat:
+            rezultat[nume] = rezultat[nume] + pret
+        else:
+            rezultat[nume] = pret
+    return rezultat
 
         
