b545331309a5369a73b2a20480ce0d85df0a07db
UrbanDacian <dacianurban13@gmail.com>
2021-11-09 00:27:45 +0200

    main

---
+++ main.py
@@ -0,0 +1,24 @@
+from Logic.CRUD import adaugaCheltuiala
+from UserInterface.Console import runMenu
+from UserInterface.command_line_console import runMenuCL
+
+def main():
+    lista = []
+    lista = adaugaCheltuiala(1, "Ion", "Business", 300, "Da", lista)
+    lista = adaugaCheltuiala(2, "Marcel", "Economy", 200, "Nu", lista)
+    while True:
+        print("1.Meniu principal")
+        print("2.Meniu secundar")
+        print("x.Inchidere")
+        menu = input("Alegeti meniul: ")
+        if menu == "1":
+            runMenu(lista)
+        elif menu == "2":
+            runMenuCL(lista)
+        elif menu == "x":
+            break
+        else:
+            print("Optiune gresita.Reincercati!")
+
+
+main()
