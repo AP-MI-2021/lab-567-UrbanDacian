76e974ebe7b5056bd6e43123ae3d32acd6178f90
UrbanDacian <dacianurban13@gmail.com>
2021-11-23 08:31:31 +0200

    Functionalitate4

--- Logic/Functionalitate4.py
+++ Logic/Functionalitate4.py
@@ -10,4 +10,4 @@ def ordonarePreturi(lista):
     :param lista: lista de dictionare
     :return: lista ordonata descrescator
     '''
-    return sorted(lista, key=obtinerePret)
+    return sorted(lista, key=obtinerePret, reverse=True)
